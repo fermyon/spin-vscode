acedb41d4378372471b0c3aa570f1b7f6d5cbf00
itowlson <ivan.towlson@fermyon.com>
2022-06-08 14:36:06 +1200

    Webpack woes
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
@@ -23,6 +24,9 @@ const extensionConfig = {
     vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
   },
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /^electron$/ })
+  ],
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: ['.ts', '.js']
