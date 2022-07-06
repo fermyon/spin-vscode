76b67bbf1d1e447466a08812fedbb02ded514fe4
itowlson <ivan.towlson@fermyon.com>
2022-07-07 11:50:50 +1200

    Handle login failures
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -18,36 +18,131 @@ export async function deploy(context: vscode.ExtensionContext) {
     }
     const [deployParameters, unsaved] = deployParameters_.value;
 
-    const deployResult = await longRunningProcess("Spin deploy in progress...", (tok) =>
-        spin.deploy(tok, deployParameters)
+    let state: DeployState | undefined = { state: 'deploying', parameters: deployParameters, unsaved, context };
+
+    while (state !== undefined) {
+        state = await runDeployStateMachine(state);
+    }
+}
+
+// The login failure flows were breaking my brain so time to overengineer
+// the heck out of this mofo
+
+// * deploying -> succeeded
+//                | redeploying [if already exists]
+//                | getting new credentials [if login failed]
+//                | failed
+//   redeploying -> succeeded
+//                  | getting new credentials [if login failed]
+//                  | failed
+//   getting new credentials -> redeploying [if new creds given, always redeploy because bindle upload would already have happened by this point]
+//                              | TERMINATE [if new creds prompt cancelled]
+//   succeded -> TERMINATE
+//   failed -> TERMINATE
+
+type DeployState =
+    { state: 'deploying', parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext } |
+    { state: 'succeeded', parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean, message: string } |
+    { state: 'failed', message: string, existedBefore: boolean } |
+    { state: 'redeploying', parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean } |
+    { state: 'getting-new-credentials', parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean };
+
+async function runDeployStateMachine(state: DeployState): Promise<DeployState | undefined> {
+    switch (state.state) {
+        case 'deploying':
+            return await runDeploy(state.parameters, state.unsaved, state.context);
+        case 'succeeded':
+            return await runSucceeded(state.parameters, state.unsaved, state.context, state.existedBefore, state.message);
+        case 'failed':
+            return await runFailed(state.message, state.existedBefore);
+        case 'redeploying':
+            return await runRedeploy(state.parameters, state.unsaved, state.context, state.existedBefore);
+        case 'getting-new-credentials':
+            return await runGettingNewCredentials(state.parameters, state.unsaved, state.context, state.existedBefore);
+        default:
+            cantHappen(state);
+    }
+}
+
+async function runDeploy(parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext): Promise<DeployState> {
+    const result = await longRunningProcess("Spin deploy in progress...", (tok) =>
+        spin.deploy(tok, parameters)
     );
 
-    if (isOk(deployResult)) {
-        // should we understand it too?
-        output.appendLine(deployResult.value);
-        output.show();
-        await notifyDeploymentComplete(unsaved, "deployment", context, deployParameters);
-    } else {
-        output.appendLine(deployResult.message);
-        const alreadyExists = deployResult.message.includes('already exists on the server');
-        if (alreadyExists) {
-            const reactivateResult = await longRunningProcess("Deployment exists, reactivating...", (tok) => 
-                spin.deploy(tok, deployParameters, true)
-            );
-            if (isOk(reactivateResult)) {
-                output.appendLine(reactivateResult.value);
-                output.show();
-                await notifyDeploymentComplete(unsaved, "deployment (reactivation)", context, deployParameters);
-            } else {
-                output.appendLine(reactivateResult.message);
-                output.show();
-                await vscode.window.showErrorMessage("Spin deployment (reactivation) failed. See Output pane for details.");
-            }
-        } else {
-            output.show();
-            await vscode.window.showErrorMessage("Spin deployment failed. See Output pane for details.");
-        }
+    if (isOk(result)) {
+        return { state: 'succeeded', parameters, unsaved, context, existedBefore: false, message: result.value };
     }
+
+    if (result.message.includes('already exists on the server')) {
+        output.appendLine(result.message);
+        return { state: 'redeploying', parameters, unsaved, context, existedBefore: true };
+    }
+
+    if (result.message.includes('Login failed')) {
+        output.appendLine(result.message);
+        return { state: 'getting-new-credentials', parameters, unsaved, context, existedBefore: false };
+    }
+
+    return { state: 'failed', message: result.message, existedBefore: false };
+}
+
+async function runSucceeded(parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean, message: string): Promise<undefined> {
+    output.appendLine(message);
+    output.show();
+    const description = existedBefore ? "deployment (reactivation)" : "deployment";
+    await notifyDeploymentComplete(unsaved, description, context, parameters);
+    return undefined;
+}
+
+async function runFailed(message: string, existedBefore: boolean): Promise<undefined> {
+    output.appendLine(message);
+    output.show();
+    const description = existedBefore ? "deployment (reactivation)" : "deployment";
+    await vscode.window.showErrorMessage(`Spin ${description} failed. See Output pane for details.`);
+    return undefined;    
+}
+
+async function runRedeploy(parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean): Promise<DeployState> {
+    const title = existedBefore ?
+        "Spin deploy in progres..." :
+        "Deployment exists, reactivating...";
+
+    const result = await longRunningProcess(title, (tok) =>
+        spin.deploy(tok, parameters, true)
+    );
+
+    if (isOk(result)) {
+        return { state: 'succeeded', parameters, unsaved, context, existedBefore, message: result.value };
+    }
+
+    if (result.message.includes('Login failed')) {
+        output.appendLine(result.message);
+        return { state: 'getting-new-credentials', parameters, unsaved, context, existedBefore: existedBefore };
+    }
+
+    return { state: 'failed', message: result.message, existedBefore: existedBefore };
+}
+
+async function runGettingNewCredentials(parameters: spin.DeployParameters, unsaved: UnsavedEnvironmentInfo, context: vscode.ExtensionContext, existedBefore: boolean): Promise<DeployState | undefined> {
+    const prompt = `Login failed. Please enter password for ${parameters.hippoUsername}`;
+    const password = await vscode.window.showInputBox({ prompt, password: true });
+    if (!password) {
+        return undefined;
+    }
+
+    const newParameters = { ...parameters, hippoPassword: password };
+
+    // What is now unsaved, given this new password?  If we were already in
+    // 'everything unsaved' (i.e. no environment) then that's still the case.
+    // If we were in already in 'password unsaved' (i.e. environment without
+    // password) then that's till the case too!  But if we were in 'nothing
+    // unsaved' (i.e. environment with password) then we need to go to 'password
+    // unsaved'.
+    const newUnsaved: UnsavedEnvironmentInfo = (unsaved.toSave === 'none') ?
+        { toSave: 'password', envName: unsaved.envName } :
+        unsaved;
+
+    return { state: 'redeploying', parameters: newParameters, unsaved: newUnsaved, context, existedBefore };
 }
 
 async function notifyDeploymentComplete(unsaved: UnsavedEnvironmentInfo, description: string, context: vscode.ExtensionContext, deployParameters: spin.DeployParameters) {
@@ -87,7 +182,7 @@ async function completeDeployParameters(context: vscode.ExtensionContext, fermyo
     if (fermyonEnv) {
         const hippoPassword = await getHippoPassword(context, fermyonEnv.name);
         if (hippoPassword) {
-            return accepted([{ hippoPassword, ...fermyonEnv}, { toSave: 'none'} ]);
+            return accepted([{ hippoPassword, ...fermyonEnv}, { toSave: 'none', envName: fermyonEnv.name }]);
         } else {
             const hippoPassword = await vscode.window.showInputBox({ prompt: `Hippo password for ${fermyonEnv.name}`, password: true });
             if (hippoPassword) {
@@ -140,4 +235,4 @@ async function envOrPrompt(env: string, promptOpts: vscode.InputBoxOptions): Pro
 type UnsavedEnvironmentInfo =
     { toSave: 'all' } |
     { toSave: 'password', envName: string } |
-    { toSave: 'none' };
+    { toSave: 'none', envName: string };
