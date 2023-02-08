75c755ad61325d10c18201fdaea2eeaaff3b80a3
itowlson <ivan.towlson@fermyon.com>
2023-02-08 15:42:09 +1300

    Use Spin layout convention
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- src/layout.ts
+++ src/layout.ts
@@ -1,27 +1,12 @@
 import * as path from 'path';
+import xdg = require('xdg-portable/cjs');
+
+const EXTENSION_FOLDER = "spin-vscode";
 
 export function toolsFolder(): string {
-    return path.join(home(), `.fermyon/spin-vscode/tools`);
+    return path.join(cacheFolder(), `tools`);
 }
 
-export function dataFolder(): string {
-    return path.join(home(), `.fermyon/spin-vscode/data`);
-}
-
-function home(): string {
-    return process.env['HOME'] ||
-        concatIfSafe(process.env['HOMEDRIVE'], process.env['HOMEPATH']) ||
-        process.env['USERPROFILE'] ||
-        '';
-}
-
-function concatIfSafe(homeDrive: string | undefined, homePath: string | undefined): string | undefined {
-    if (homeDrive && homePath) {
-        const safe = !homePath.toLowerCase().startsWith('\\windows\\system32');
-        if (safe) {
-            return homeDrive.concat(homePath);
-        }
-    }
-
-    return undefined;
+function cacheFolder(): string {
+    return path.join(xdg.cache(), EXTENSION_FOLDER);
 }
